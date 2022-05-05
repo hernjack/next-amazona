19901873a802080241cc14ea8c3dbe1399a843ba
Felipe Rojas <andres.rhernandez@mercadolibre.com.co>
2022-05-05 11:02:52 -0500

    Added products and produc detail. also mongoose conection

--- components/Layout.js
+++ components/Layout.js
@@ -1,27 +1,46 @@
 import React from 'react';
-import Head from 'next/head'
-import { AppBar, Toolbar, Typography, Container } from '@mui/material';
+import Head from 'next/head';
+import NextLink from 'next/link';
+import { AppBar, Toolbar, Typography, Container, Link } from '@mui/material';
 import useStyles from '../utils/styles';
-import classes from './Layout.module.css';
+import classes from '../styles/Layout.module.css';
 
 const Layout = (props) => (
   <div className={classes.Layout}>
     <Head>
       <title>Amazona Clone</title>
+      <title>{props.title ? `${props.title} - Next Amazona` : 'Next Amazona'}</title>
+      {props.description && <meta name="description" content={props.description}></meta>}
     </Head>
     <AppBar position='static' sx={useStyles.navbar}>
       <Toolbar>
-        <Typography>Amazona</Typography>
+        <NextLink href="/" passHref>
+          <Link >
+            <Typography className={classes['main-link']}>Amazona</Typography>
+          </Link>
+        </NextLink>
+        <div className={classes.grow}></div>
+        <div className={classes.links}>
+          <NextLink href="/cart" passHref>
+            <Link >
+              <Typography>Cart</Typography>
+            </Link>
+          </NextLink>
+          <NextLink href="/login" passHref>
+            <Link >
+              <Typography>Login</Typography>
+            </Link>
+          </NextLink>
+        </div>
       </Toolbar>
     </AppBar>
     <Container sx={useStyles.main}>
       {props.children}
     </Container>
-    <footer sx={useStyles.footer}>
+    <footer>
       <Typography>All rights reserved. Amazona Clone. Jayson Hernandez. 2022</Typography>
     </footer>
   </div>
 )
-
 
 export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,33 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import React from 'react';
+import NextLink from 'next/link';
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Grid, Typography } from '@mui/material'
 import Layout from '../components/Layout'
-import styles from '../styles/Home.module.css'
+import data from '../utils/data'
 
 export default function Home() {
   return (
     <Layout>
-        <h1>Products</h1>
-        <ul>
-          <li>product 1</li>
-          <li>product 2</li>
-          <li>product 3</li>
-        </ul>
+      <h1>Products</h1>
+      <Grid container spacing={3}>
+        {data.products.map((product) => (
+          <Grid item md={4} key={product.name}>
+            <Card>
+              <NextLink href={`/product/${product.slug}`} passHref>
+                <CardActionArea>
+                  <CardMedia component="img" image={product.image} title={product.name}></CardMedia>
+                  <CardContent>
+                    <Typography>{product.name}</Typography>
+                  </CardContent>
+                </CardActionArea>
+              </NextLink>̦
+              <CardActions>
+                <Typography>${product.price}</Typography>
+                <Button size='small' color='primary'>Add to cart</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </Layout>
   )
 }
